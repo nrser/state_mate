cc57dd699dd0d1d8a708e94bfaff355a3b35eff0
nrser <neil@ztkae.com>
2015-10-19 01:24:18 +0800

    unfinished work on launchd adapter

--- lib/state_mate/adapters/launchd.rb
+++ lib/state_mate/adapters/launchd.rb
@@ -10,13 +10,63 @@ require 'state_mate/adapters/defaults'
 using NRSER
 
 # very useful:
-# 
+#
 # <http://launchd.info/>
 # 46
 
 module StateMate::Adapters::LaunchD
 
   EXE = '/bin/launchctl'
+
+  # adapter API methods
+  # ===================
+
+  # @api adapter
+  #
+  def self.read key, options = {}
+    file_path, key_segs = parse_key key
+
+    # get the hash of the plist at the file path and use that to get the label
+    plist = CFPropertyList::List.new file: file_path
+    data = CFPropertyList.native_types plist.value
+    label = data["Label"]
+
+    case key_segs
+    # the only thing we can handle right now
+    when ['Disabled']
+      disabled? label
+    else
+      raise "unprocessable key: #{ key.inspect }"
+    end
+  end
+
+
+  # @api adapter
+  #
+  def self.write key, value, options = {}
+    file_path, key_segs = parse_key key
+
+    case key_segs
+    when ['Disabled']
+      case value
+      when true
+        unload file_path
+
+      when false
+        load file_path
+
+      else
+        raise StateMate::Error::TypeError value, "expected true or false"
+
+      end
+    else
+      raise "unprocessable key: #{ key.inspect }"
+    end
+  end
+
+
+  # util methods
+  # ============
 
   def self.truncate_values hash, length
     hash.map {|k, v|
@@ -40,9 +90,35 @@ module StateMate::Adapters::LaunchD
     end
   end
 
+  # @api private
+  #
+  # @return [Hash<String, Hash>] the user's overrides, which looks to be a hash
+  #     of string labels to hashes of properties, one of which is a 'Disabled'
+  #     key.
   def self.user_overrides_db user = ENV['USER']
-    plist = CFPropertyList::List.new file: user_overrides_db_path(user)
-    CFPropertyList.native_types plist.value
+    db_path = user_overrides_db_path(user)
+    if File.exists? db_path
+      plist = CFPropertyList::List.new file: user_overrides_db_path(user)
+      CFPropertyList.native_types plist.value
+    else
+      {}
+    end
+  end
+
+  # @param user [String] the username.
+  # @return [Fixnum] the user's integer id.
+  def self.uid user
+    Cmds!("id -u %{user}", user: user).out.chomp.to_i
+  end
+
+  def self.new_disabled user = ENV['USER']
+    data = Cmds(
+      "%{exe} print-disabled user/%{uid}",
+      exe: EXE,
+      uid: uid(user),
+    ).out
+
+    list = CFPropertyList::List.new data: data
   end
 
   def self.disabled? label, user = ENV['USER']
@@ -68,43 +144,5 @@ module StateMate::Adapters::LaunchD
   def self.unload file_path
     Cmds! "%{exe} unload -w %{file_path}",  exe: EXE,
                                             file_path: file_path
-  end
-
-  def self.read key, options = {}
-    file_path, key_segs = parse_key key
-
-    # get the hash of the plist at the file path and use that to get the label
-    plist = CFPropertyList::List.new file: file_path
-    data = CFPropertyList.native_types plist.value
-    label = data["Label"]
-
-    case key_segs
-    # the only thing we can handle right now
-    when ['Disabled']
-      disabled? label
-    else
-      raise "unprocessable key: #{ key.inspect }"
-    end
-  end
-
-  def self.write key, value, options = {}
-    file_path, key_segs = parse_key key
-
-    case key_segs
-    when ['Disabled']
-      case value
-      when true
-        unload file_path
-
-      when false
-        load file_path
-
-      else
-        raise StateMate::Error::TypeError value, "expected true or false"
-
-      end
-    else
-      raise "unprocessable key: #{ key.inspect }"
-    end
   end
 end # LaunchD
