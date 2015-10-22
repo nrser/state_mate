4c5fa455797127697924ada9e5bb664777f55279
nrser <neil@ztkae.com>
2015-10-22 16:07:56 +0800

    use unique name for test in register spec

--- spec/state_mate/adapters/register_spec.rb
+++ spec/state_mate/adapters/register_spec.rb
@@ -10,8 +10,9 @@ describe 'StateMate::Adapters.register' do
   end
   
   it "should key registered object under it's name in @@index" do
-    name = 'blah'
     obj = Object.new
+    # unique name since reg is global and persistent
+    name = obj.object_id.to_s
     adapters.register name, obj
     index = adapters.class_variable_get :@@index
     expect(index).to be_instance_of Hash
